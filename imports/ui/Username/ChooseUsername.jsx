bf18c93dc1c2972d5b602ad3b13e94dd19adf9ae
Samuel Blake <samuelblake97@gmail.com>
2019-02-28 09:49:56 -0800

    Choose Username page

---
+++ imports/ui/Username/ChooseUsername.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Wrapper from '../Wrapper/Wrapper'
+import '../assets/_main.scss';
+
+
+export default class ChooseUsername extends Component {
+  // static propTypes = {
+  //   prop: PropTypes
+  // }
+
+  //TODO: re-render when username is set
+
+  render() {
+    return (
+      <Wrapper>
+        <h1 id="title-dynamic">Dynamic!</h1>
+        <form id="username-form">
+          <div id="username" className="field-container">
+            <label className="field-title" htmlFor="username">Please enter an appropriate username</label>
+            <div className="input-container">
+              <input type="text" name="username" placeholder="Type your username here" id="username"/>
+            </div>
+          </div>
+        </form>
+      </Wrapper>
+    )
+  }
+}
