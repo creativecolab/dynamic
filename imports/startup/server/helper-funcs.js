4b83822a7224dcea7c64d360a788046265a91a97
Samuel Blake <samuelblake97@gmail.com>
2019-08-12 14:09:39 -0700

    average rating of an interaction

--- imports/startup/server/helper-funcs.js
+++ imports/startup/server/helper-funcs.js
@@ -44,3 +44,21 @@ export function calculateDuration(activity) {
 
   return -1;
 }
+
+// parse the preferences of a user to get the average rating for a person
+export function getAverageRating(participant, participant_preferences, other_participant, other_participant_preferences, activities) {
+
+  // worst javascript in the world to get the ratings that pertain to the person of interest
+  let ratings_for_person = participant_preferences.filter((preference) => activities.includes(preference.activity_id))
+                                      .map((rating) => rating.values.filter((value) => value.pid === other_participant))
+                                      .map((person_rating) => person_rating[0].value);
+  let participant_avg = parseFloat((ratings_for_person.reduce((total, value) => total + value, 0) / ratings_for_person.length).toFixed(3));
+
+  // more terrible javascript to get ratings from the person of interest of the participant
+  let ratings_for_participant = other_participant_preferences.filter((preference) => activities.includes(preference.activity_id))
+                                      .map((rating) => rating.values.filter((value) => value.pid === participant))
+                                      .map((person_rating) => person_rating[0].value);
+  let other_participant_avg = parseFloat((ratings_for_participant.reduce((total, value) => total + value, 0) / ratings_for_participant.length).toFixed(3));
+
+  return ((participant_avg + other_participant_avg) / 2).toFixed(3); 
+}
