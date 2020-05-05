53bdef4d0bd2f58428c06808491cf041500bddbe
Samuel Blake <samuelblake97@gmail.com>
2020-05-05 16:22:52 -0700

    EmailConfirmation Page
    -saving email
    -displaying selected names
    -reorganization
    -also has additional api change

--- imports/ui/Handlers/SummaryHandler/SummaryHandler.jsx
+++ imports/ui/Handlers/SummaryHandler/SummaryHandler.jsx
@@ -8,7 +8,7 @@ import Teams from '../../../api/teams';
 import Mobile from '../../Layouts/Mobile/Mobile';
 
 import ActivityCompletion from '../../Summary/ActivityCompletion';
-import TableSummary from '../../Summary/Table/TableSummary';
+import TableSummary from '../../Summary/TableSummary';
 import EmailConfirmation from '../../Summary/EmailConfirmation';
 import Loading from '../../Components/Loading/Loading';
 import Survey from '../../Components/Survey/Survey';
@@ -21,20 +21,73 @@ class SummaryHandler extends Component {
 
   constructor(props) {
     super(props);
-    const { viewedSummary, selectedEmails, sentEmails } = this.props;
+    const { viewedSummary, selectedEmails, sentEmails, sendEmailsTo } = this.props;
 
     this.state = {
-      viewedSummary: viewedSummary,
-      selectedEmails: selectedEmails,
-      sentEmails: sentEmails
+      viewedSummary: viewedSummary ? viewedSummary : false,
+      selectedEmails: selectedEmails ? selectedEmails : false,
+      sentEmails: sentEmails ? sentEmails : false,
+      recipients: sendEmailsTo ? sendEmailsTo.map(pid => Users.findOne({ pid: pid }).name) : [],
+      userEmail: "",
+      invalidEmail: false
     }
   }
 
+  /* Functions for child components to use to update the states here */
+
+  // for table summary
+  trackRecipients = (recipient, add) => {
+    // add or remove recipients
+    if (add) {
+      // only add a recipient once
+      // if (this.state.recipients) {
+      //   this.setState({
+      //     recipients: [recipient]
+      //   });
+      // } else 
+      if (this.state.recipients.indexOf(recipient) == -1) {
+        this.setState({
+          recipients: [...this.state.recipients, recipient]
+        });
+      }
+    } else {
+      // remove this recipient
+      this.setState({
+        recipients: this.state.recipients.filter(pot_rec => (pot_rec != recipient))
+      });
+    }
+
+  }
+
+  // for email confirmation page
+  checkEmailToConfirm = () => {
+    const { pid, session_id } = this.props;
+    const { userEmail } = this.state;
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(userEmail)) {
+      // valid email
+      this.setState({
+        invalidEmail: false,
+        sentEmails: true
+      });
+      Meteor.call('users.saveEmail', pid, session_id, userEmail, () => {
+        console.log("Accepted email address.");
+      });
+    } else {
+      this.setState({
+        invalidEmail: true,
+      });
+
+    }
+  }
+
+  /* Component Selection */
+
   renderBody() {
     // determine with component to load based on state
-    const { viewedSummary, selectedEmails, sentEmails } = this.state;
+    const { viewedSummary, selectedEmails, sentEmails, userEmail, invalidEmail, recipients } = this.state;
 
-    const { preferences, pid, session_id } = this.props;
+    const { pid, session_id } = this.props;
 
 
     // haven't looked at the summary yet
@@ -43,29 +96,24 @@ class SummaryHandler extends Component {
 
     // haven't finished selecting emails
     if (!selectedEmails)
-      return (<TableSummary preferences={preferences} pid={pid} session_id={session_id} />);
+      return (<TableSummary pid={pid} session_id={session_id} trackRecipients={this.trackRecipients.bind(this)} />);
 
     if (!sentEmails)
-      return (<EmailConfirmation preferences={preferences} onSubmit={() => this.setState({ sentEmails: true })} />);
-  }
-
-  backButtonPressed() {
-    console.log("back pressed", Date());
-    console.log(this.state);
+      return (
+        <EmailConfirmation
+          recipients={recipients}
+          trackEmail={(email) => { this.setState({ userEmail: email, invalidEmail: false }); }}
+          onSubmit={this.checkEmailToConfirm}
+          invalidEmail={invalidEmail}
+          userEmail={userEmail}
+        />);
   }
 
   render() {
     // get props from parent
-    const { pid, session_id, preferences } = this.props;
+    const { pid, session_id } = this.props;
 
-    // get 'viewSummary', preferences, 'selectedEmails', 'sentEmails' 
     const { viewedSummary, selectedEmails, sentEmails } = this.state;
-    console.log(viewedSummary);
-    console.log(preferences);
-    console.log(selectedEmails);
-    console.log(sentEmails);
-
-
 
     // pid is required from parent
     if (!pid) return <Loading />;
@@ -77,7 +125,11 @@ class SummaryHandler extends Component {
     let buttonAct, backBtnAct;
     if (!viewedSummary) {
       buttonAct = () => {
-        this.setState({ viewedSummary: true });
+        let recipients = this.props.sendEmailsTo.map(pid => Users.findOne({ pid: pid }).name)
+        this.setState({
+          viewedSummary: true,
+          recipients: recipients
+        });
         Meteor.call('users.toggleViewedSummary', pid, session_id, true, () => {
           console.log("set viewSummary to ", true);
         });
@@ -92,12 +144,7 @@ class SummaryHandler extends Component {
       }
     }
     else {
-      buttonAct = () => {
-        this.setState({ sentEmails: true });
-        Meteor.call('users.toggleSentEmails', pid, session_id, true, () => {
-          console.log("set sentEmails to ", true);
-        });
-      }
+      buttonAct = this.checkEmailToConfirm.bind(this);
       backBtnAct = () => {
         this.setState({ selectedEmails: false });
         Meteor.call('users.toggleSelectedEmails', pid, session_id, false, () => {
@@ -114,10 +161,10 @@ class SummaryHandler extends Component {
         hasNavbar={true}
         hasTimer={false}
         displayTeam={false}
-        hasBackBtn={this.state.selectedEmails ? true : false}
         buttonAction={buttonAct}
         buttonSize={'medium'}
         buttonTxt={'Send Emails'}
+        hasBackBtn={this.state.selectedEmails ? true : false}
         backBtnAction={backBtnAct}
       >
         {this.renderBody()}
@@ -129,7 +176,7 @@ class SummaryHandler extends Component {
 // updates component when activity changes
 export default withTracker(props => {
 
-  var viewedSummary, selectedEmails, sentEmails, preferences;
+  var viewedSummary, selectedEmails, sentEmails, sendEmailsTo;
   const user = Users.findOne({ pid: props.pid });
 
   if (user) {
@@ -140,10 +187,10 @@ export default withTracker(props => {
         viewedSummary = user.sessionHistory[i].viewedSummary;
         selectedEmails = user.sessionHistory[i].selectedEmails;
         sentEmails = user.sessionHistory[i].sentEmails;
+        sendEmailsTo = user.sessionHistory[i].sendEmailsTo;
       }
     }
-    preferences = user.preferences;
   }
 
-  return { user, preferences, viewedSummary, selectedEmails, sentEmails };
+  return { user, viewedSummary, selectedEmails, sentEmails, sendEmailsTo };
 })(SummaryHandler);
--- imports/ui/Summary/TableComponents/DetailsList.jsx
+++ imports/ui/Summary/TableComponents/DetailsList.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class DetailsList extends Component {
+
+  static proptypes = {
+    questions: PropTypes.array.isRequired
+  }
+
   constructor(props) {
     super(props);
     this.state = ({
